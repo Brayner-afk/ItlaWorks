272326a8dd2866e690e3d6e404dee6ee74d2e8bb
Brayner-afk <20250933@itla.edu.do>
2025-10-12 10:00:07 -0400

    Adding my second homework

---
+++ Tarea 2/Tareas/Tarea 2.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("--- Even or Odd Checker ---");
+Console.Write("Enter a whole number (integer): ");
+
+string userInput = Console.ReadLine();
+int numberToCheck = int.Parse(userInput);
+int remainder = numberToCheck % 2;
+
+if (remainder == 0)
+{
+    Console.WriteLine("\nResult: The number " + numberToCheck + " is EVEN.");
+}
+else
+{
+    Console.WriteLine("\nResult: The number " + numberToCheck + " is ODD.");
+}
+Console.ReadKey();
