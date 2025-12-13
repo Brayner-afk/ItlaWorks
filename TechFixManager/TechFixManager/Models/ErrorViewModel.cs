2463f270d5bef2eeee4156fc1510419c2117e817
Brayner-afk <20250933@itla.edu.do>
2025-12-13 09:32:51 -0400

    delited
    cause of mistakes

--- TechFixManager/TechFixManager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TechFixManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
