8f7cb9e7b845f4d06950589bf9c35ff0cca22c1b
Brayner-afk <20250933@itla.edu.do>
2025-12-13 07:28:03 -0400

    Final Project
    This is my final project - TechFixManager

---
+++ TechFixManager/TechFixManager/Models/Equipo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechFixManager.Models
+{
+    public class Equipo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Marca { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Required]
+        public string Serie { get; set; }
+
+        [Required]
+        public int ClienteId { get; set; }
+
+        public Cliente? Cliente { get; set; }
+    }
+}
