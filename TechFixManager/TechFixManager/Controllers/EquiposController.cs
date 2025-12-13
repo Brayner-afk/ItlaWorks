e63169e2a846b7e7ab3adb8f13e3ad5b8e5658a3
Brayner-afk <20250933@itla.edu.do>
2025-12-13 05:56:30 -0400

    Final Project
    This is my final project, TeachFixManager

---
+++ TechFixManager/TechFixManager/Controllers/EquiposController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TechFixManager.Data;
+using TechFixManager.Models;
+
+namespace TechFixManager.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EquiposController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var equipos = _context.Equipos.Include(e => e.Cliente);
+            return View(await equipos.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var equipo = await _context.Equipos
+                .Include(e => e.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (equipo == null) return NotFound();
+
+            return View(equipo);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Nombre");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(equipo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Nombre", equipo.ClienteId);
+            return View(equipo);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var equipo = await _context.Equipos.FindAsync(id);
+            if (equipo == null) return NotFound();
+
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Nombre", equipo.ClienteId);
+            return View(equipo);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Equipo equipo)
+        {
+            if (id != equipo.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Equipos.Any(e => e.Id == equipo.Id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Nombre", equipo.ClienteId);
+            return View(equipo);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var equipo = await _context.Equipos
+                .Include(e => e.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (equipo == null) return NotFound();
+
+            return View(equipo);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var equipo = await _context.Equipos.FindAsync(id);
+            if (equipo != null)
+            {
+                _context.Equipos.Remove(equipo);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
