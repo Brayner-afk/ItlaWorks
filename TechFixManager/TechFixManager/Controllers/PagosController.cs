5b722b71714de8dd145844a8623dda3b3f989104
Brayner-afk <20250933@itla.edu.do>
2025-12-13 09:33:48 -0400

    Final Projetc
    This is My Final Project / TechFixManager

---
+++ TechFixManager/TechFixManager/Controllers/PagosController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechFixManager.Data;
+using TechFixManager.Models;
+
+namespace TechFixManager.Controllers
+{
+    public class PagosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PagosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // INDEX
+        public IActionResult Index()
+        {
+            return View(_context.Pagos.ToList());
+        }
+
+        // CREATE (GET)
+        public IActionResult Create()
+        {
+            var pago = new Pago
+            {
+                FechaPago = DateTime.Now
+            };
+            return View(pago);
+        }
+
+        // CREATE (POST)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pago pago)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Pagos.Add(pago);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(pago);
+        }
+
+        // DETAILS
+        public IActionResult Details(int id)
+        {
+            var pago = _context.Pagos.Find(id);
+            if (pago == null)
+                return NotFound();
+
+            return View(pago);
+        }
+
+        // DELETE
+        public IActionResult Delete(int id)
+        {
+            var pago = _context.Pagos.Find(id);
+            if (pago == null)
+                return NotFound();
+
+            return View(pago);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var pago = _context.Pagos.Find(id);
+            _context.Pagos.Remove(pago);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ TechFixManager/TechFixManager/Models/Repuesto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechFixManager.Models
+{
+    public class Repuesto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre del repuesto")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+
+        [Required]
+        [Display(Name = "Cantidad disponible")]
+        public int Cantidad { get; set; }
+
+        [Required]
+        [Display(Name = "Precio")]
+        [DataType(DataType.Currency)]
+        public decimal Precio { get; set; }
+    }
+}
