6c9ce7355d4ed056a36a5b5bc6f0a4eb1bc454f7
Brayner-afk <20250933@itla.edu.do>
2025-12-13 07:23:34 -0400

    changes
    deleted because of changes

--- TechFixManager/TechFixManager/Models/Tecnico.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TechFixManager.Models
-{
-    public class Tecnico
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Especialidad { get; set; }
-    }
-}
